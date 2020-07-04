e53822632d5b9d2170348475885e6a11af3ffc63
Gurukulpoint <Gurukulpoint@Dell>
2020-07-04 09:10:12 +0530

    Commit Maven project

---
+++ src/test/java/Com/qa/test/Testmaven.java
@@ -0,0 +1,23 @@
+package Com.qa.test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Testmaven {
+
+	
+	@Test
+	
+	public void  demo() {
+		
+		System.setProperty("webdriver.gecko.driver", "E:\\software\\geckodriver-v0.20.1-win64\\geckodriver.exe");
+
+		WebDriver driver = new FirefoxDriver();
+			               driver.navigate().to("http://result.rgpv.ac.in/result/programselect.aspx");
+							driver.findElement(By.xpath(".//*[@id='radlstProgram_0']")).click();
+
+	}
+	
+	
+}
