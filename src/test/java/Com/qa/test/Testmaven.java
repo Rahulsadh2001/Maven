7ee8d12783c6190b956f485d2e43ab22c83ded90
Rahul sadh <44499511+Rahulsadh2001@users.noreply.github.com>
2023-08-01 12:09:13 +0530

    Update Testmaven.java

--- src/test/java/Com/qa/test/Testmaven.java
+++ src/test/java/Com/qa/test/Testmaven.java
@@ -14,6 +14,7 @@ public class Testmaven {
 		System.setProperty("webdriver.gecko.driver", "E:\\software\\geckodriver-v0.20.1-win64\\geckodriver.exe");
 
 		WebDriver driver = new FirefoxDriver();
+		Thread.sleep(5000);
 			               driver.navigate().to("http://result.rgpv.ac.in/result/programselect.aspx");
 							driver.findElement(By.xpath(".//*[@id='radlstProgram_0']")).click();
 		    driver.navigate().to("http://result.rgpv.ac.in/result/programselect.aspx");
